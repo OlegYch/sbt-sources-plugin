a8b57c17056cde7e44d034026d0473c2e7d09b24
OlegYch <OlegYch@.(none)>
2011-03-04 00:59:11 +0200

    added failing test

---
+++ sbt-sources-tests/src/sbt-test/sources-plugin/duplicate-artifacts/project/build/ScriptedTestProject.scala
@@ -0,0 +1,25 @@
+import collection.immutable.TreeSet
+import sbt._
+
+class ScriptedTestProject(info: ProjectInfo)
+        extends DefaultProject(info) with ScriptedTestAssertTasks with ProjectWithSources with
+        ExpectedManagedLib {
+
+  override def libraryDependencies = Set(
+    "org.seleniumhq.selenium" % "selenium-server" % "2.0a6" % "compile" withSources
+    , "commons-lang" % "commons-lang" % "2.5" % "compile" withSources
+  )
+
+  val compileEntries = TreeSet(
+    "xml-apis-1.3.04.jar",
+    "xml-apis-1.3.04-sources.jar",
+    "serializer-2.7.1.jar",
+    "commons-io-1.4-sources.jar",
+    "commons-io-1.4.jar",
+    "commons-lang-2.5-sources.jar",
+    "commons-lang-2.5.jar",
+    "xml-apis-1.3.04.jar"
+  )
+
+  val testEntries = TreeSet[String]()
+}
---
+++ sbt-sources-tests/src/sbt-test/sources-plugin/duplicate-artifacts/project/plugins/Plugins.scala
@@ -0,0 +1,8 @@
+import sbt._
+
+class Plugins(info: ProjectInfo) extends PluginDefinition(info) {
+  val testedVersion = "0.2.0-SNAPSHOT"
+  val groupId = "com.olegych"
+  val sourcesPlugin = groupId % "sbt-sources-plugin" % testedVersion
+  val scriptedTestUtils = groupId % "sbt-sources-tests_2.7.7" % testedVersion
+}
